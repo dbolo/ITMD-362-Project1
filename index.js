19b9d62c9558e9c162d7a978fc4bfe219298a18e
dbolo <31314227+dbolo@users.noreply.github.com>
2018-02-05 16:55:20 -0600

    editing work

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ $('#event-invitation').on('submit', function(e) { //someone wants to submit
   e.preventDefault();
 
   console.log("first name: " + $('#fname').val());
+
+  console.log("phone no: "+$('#phone').val());
+
+  if($('#phone').val().length <= 0)
+  {
+    alert("Phone number is empty!");
+  }
+  else
+  {
+    // do something as phone number is a valid phone number
+  }
+
+  console.log("Someone tried to submit the form!!!");
+});
