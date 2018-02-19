e272896bf5d75be9a2dd92e62a87e84e7a426677
dbolo <31314227+dbolo@users.noreply.github.com>
2018-02-18 18:54:47 -0600

    editing

--- index.js
+++ index.js
@@ -1,43 +1,56 @@
-$('#event-invitation').on('submit', function(e) { //someone wants to submit
-  e.preventDefault();
+$(document).ready(function(){
 
-  console.log("first name: " + $('#fname').val());
+console.log("document is ready.");
 
-  console.log("phone no: "+$('#phone').val());
-
-  if($('#phone').val().length <= 0)
+  function record_problem(problem)
   {
-    alert("Phone number is empty!");
+    $('#problems > ul').append('<li>'+problem+"</li>");
+  }
+
+  $('#event-invitation').on('submit', function(e) { //someone wants to submit
+    e.preventDefault();
+
+    $('#problems > ul').empty();
+
+    console.log("first name: " + $('#fname').val());
+
+    console.log("phone no: "+$('#phone').val());
+
+    if($('#phone').val().length <= 0)
+    {
+      record_problem("Phone number is empty!");
+    }
+    else
+    {
+      // do something as phone number is a valid phone number
+    }
+
+    if($('#fname').val().length <= 0)
+    {
+      record_problem('#fname is empty!');
+    }
+    else
+    {
+      // do something as phone number is a valid phone number
+    }
+
+
+
+  if($('#lname').val().length <= 0)
+  {
+    record_problem('#lname is empty!');
   }
   else
   {
-    // do something as phone number is a valid phone number
+    record_problem ('your full name is correct')
   }
 
-  if($('#fname').val().length <= 0)
+  if($('#email').val().length <= 0)
   {
-    alert('#fname is empty!');
-  }
-  else
-  {
-    // do something as phone number is a valid phone number
+    record_problem('#email is empty!');
+    console.log("Someone tried to submit the form!!!");
   }
 
+  });
 
-
-if($('#lname').val().length <= 0)
-{
-  alert('#lname is empty!');
-}
-else
-{
-  alert ('your full name is correct')
-}
-
-if($('#email').val().length <= 0)
-{
-  alert('#email is empty!');
-  console.log("Someone tried to submit the form!!!");
-}
-
-});
+})
